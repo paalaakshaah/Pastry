3523b5155242c05d3fe1e3158d5c70a028c855d4
Palak Shah <spalak12@gmail.com>
2014-10-20 22:22:21 +0530

    Update pastryActor.scala

--- src/pastryActor.scala
+++ src/pastryActor.scala
@@ -1,23 +1,22 @@
 import akka.actor.Actor
 
-class pastryActor(nodeid: String, numReq: Int) extends Actor{
+class pastryActor(nodeid: String, numReq: Int) extends Actor {
 
   def receive = {
-	//each node joins one by one  
-  	case "join" =>
-      println("Node "+ self.path + " joined")
+    //each node joins one by one  
+    case "join" =>
+      println("Node " + self.path + " joined")
       sender ! "joined"
-    
+
     case "startRouting" =>
-      println("Node "+ self.path + " started routing")
-     
+      println("Node " + self.path + " started routing")
+
     //all node join together... to be removed later
-    case "initTable" =>
+    /*case "initTable" =>
       println("Node "+ self.path + " initializing table")
-      
-      sender ! "initTableDone"
-      
+      sender ! "initTableDone"*/
+
     case _ => println("Entering default case of pastryActor")
-      
+
   }
 }
