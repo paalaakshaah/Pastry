8f3970d2ec24654f6a0dce4d3c45664e4ed16cb9
Palak Shah <spalak12@gmail.com>
2014-10-23 08:13:35 +0530

    Update masterActor.scala

--- src/masterActor.scala
+++ src/masterActor.scala
@@ -3,33 +3,58 @@ import akka.actor.ActorSystem
 import scala.util.Random
 import akka.actor.Props
 import akka.actor.ActorRef
+import scala.collection.mutable.HashSet
+
+case class Joined(nodeId: String)
 
 class masterActor(numNodes: Int, numReq: Int, mySys: ActorSystem) extends Actor {
 
-  var b = 8;
-  var lenUUID: Int = 4 //length of random string can be increased to support more actors
+  var b:Int = 2;
+  var l:Int = 8;
+  var lenUUID: Int = 1 << b //length of random string can be increased to support more actors
+  var logBaseB = (Math.log(numNodes)/Math.log(lenUUID)).toInt
+  println("lenUUID::" +lenUUID + "log::"+logBaseB)
   var i: Int = 1
+  var idHash = new HashSet[String]
   var peerList: List[ActorRef] = List()
+  var prevNID : String = "first"
   var count: Int = 0; //jugaad varible for initial phase of codeing....to be removed later on
 
   def receive = {
     //each node joins one by one
     case "init" =>
-      if (i <= numNodes) {
+      if (i == 1){
+        var nodeid: String = getRandomID(i)
+        idHash.+=(nodeid)
+        println("Hash Set::" + idHash)
+        var peer = mySys.actorOf(Props(new pastryActor(nodeid, numReq, b, l, lenUUID, logBaseB)), name = nodeid)
+        peerList = peerList :+ peer
+        i+=1
+        peer ! FirstJoin(nodeid, b, l, lenUUID)
+      } else if ((i>1)&&(i <= numNodes)) {
         //create nodes
         var nodeid: String = getRandomID(i)
-        var peer = mySys.actorOf(Props(new pastryActor(nodeid, numReq)), name = i.toString)
+        idHash.+=(nodeid)
+        var peer = mySys.actorOf(Props(new pastryActor(nodeid, numReq, b, l, lenUUID, logBaseB)), name = nodeid)
         peerList = peerList :+ peer
         i += 1
         //add nodes
-        peer ! "join"
+        /*var r = (Math.random * (peerList .length -1)).toInt
+        println("random index:::::::::::"+ r)*/
+        peer ! JoinNetwork(peerList((Math.random * (peerList .length -1)).toInt))
+        
       } else {
         for (peer <- peerList) {
-          peer ! "startRouting"
+          var msg : String = "hello"
+          peer ! startRouting(msg)
         }
       }
 
-    case "joined" =>
+    case Joined(nodeId: String) =>
+      self ! "init"
+      
+    case "Joined" =>
+      //println("Node " + NID + Joined)
       self ! "init"
 
     //all node join together
@@ -60,10 +85,28 @@ class masterActor(numNodes: Int, numReq: Int, mySys: ActorSystem) extends Actor 
   def getRandomID(i: Int): String = {
     var r = new Random();
     var sb = new StringBuffer();
-    while (sb.length() < lenUUID) {
-      sb.append(Integer.toOctalString(r.nextInt()))
+    var flag  = true
+    
+    while (flag == true){ 
+	    while (sb.length() < lenUUID) {
+	      sb.append(Integer.toOctalString(r.nextInt()))
+	      //sb.append(Integer.toString(Math.abs(Integer.parseInt(Integer.toString(r.nextInt, 4)))))
+	      //sb.append(Integer.toBinaryString(r.nextInt))
+	    }
+	    println(sb.toString().substring(0, lenUUID))
+	    if(idHash.isEmpty) {
+	      println("Randomid for first node")
+	      flag = false
+	    } else if(idHash.contains(sb.toString().substring(0, lenUUID))) {
+	      println("Randomid already taken")
+	      flag = true
+	      sb.setLength(0)
+	    } else {
+	      println("Randomid is free")
+	      flag = false
+	    }
     }
-    println(sb.toString().substring(0, lenUUID))
+    
     sb.toString().substring(0, lenUUID)
   } //getRandomID method ends here
 }
