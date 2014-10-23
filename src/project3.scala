1f1bf4b81b1bc7e825f40ad67d6433608a05b9b0
Palak Shah <spalak12@gmail.com>
2014-10-23 15:21:43 +0530

    Update project3.scala

--- src/project3.scala
+++ src/project3.scala
@@ -4,23 +4,20 @@ import akka.actor.Props
 object project3 {
 
   var numNodes: Int = 1000; //default number of nodes = 1000
-  var numReq: Int = 5 //default number of requests = 5 
-
+  var numReq: Int = 5 //default number of requests = 5
+  
   def main(args: Array[String]) {
     if (args.length == 2) {
       numNodes = args(0).toInt
       numReq = args(1).toInt
-
     } else {
       println("Usage : project3.scala <numNodes> <numRequests>")
       println("Executing default scenario with number of nodes = 1000 and number of requests = 5")
-
     }
-
+  
     var system = ActorSystem("pastry")
     var master = system.actorOf(Props(new masterActor(numNodes, numReq, system)), name = "master")
-    
     master ! "init"
-    
+  
   } //Main method ends here
 }//class ends here
